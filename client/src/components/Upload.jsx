4c334ae69359860051d42060786191c6bd9918b3
Youssef-Ahmed-Zahran <youssefzahran480@gmail.com>
2025-09-26 18:41:16 +0300

    image upload

--- client/src/components/Upload.jsx
+++ client/src/components/Upload.jsx
@@ -6,9 +6,7 @@ import { makeRequest } from "../requestMethod";
 const authenticator = async () => {
   try {
     // Perform the request to the upload authentication endpoint.
-    const response = await makeRequest.get(
-      `posts/upload-auth`
-    );
+    const response = await makeRequest.get(`/posts/upload-auth`);
 
     if (!response) {
       // If the server response is not successful, extract the error text for debugging.
